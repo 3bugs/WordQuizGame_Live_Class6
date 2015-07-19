1999d853712a2122d425286e2e45a137288e2893
promlert <promlert@gmail.com>
2015-07-19 10:23:32 +0700

    2nd commit

--- app/src/main/java/com/example/wordquizgame/GameActivity.java
+++ app/src/main/java/com/example/wordquizgame/GameActivity.java
@@ -1,24 +1,79 @@
 package com.example.wordquizgame;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class GameActivity extends ActionBarActivity {
 
+    private static final String TAG = "GameActivity";
+
     private int difficulty;
+    private int numChoices;
+
+    private ArrayList<String> fileNameList;
+    private ArrayList<String> quizWordList;
+    private ArrayList<String> choiceWords;
+
+    private String answerFileName;
+    private int totalGuesses;
+    private int score;
+
+    private Random random;
+    private Handler handler;
+
+    private TextView questionNumberTextView;
+    private ImageView questionImageView;
+    private TableLayout buttonTableLayout;
+    private TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        questionNumberTextView = (TextView) findViewById(R.id.questionNumberTextView);
+        questionImageView = (ImageView) findViewById(R.id.questionImageView);
+        buttonTableLayout = (TableLayout) findViewById(R.id.buttonTableLayout);
+        answerTextView = (TextView) findViewById(R.id.answerTextView);
+
         Intent intent = getIntent();
         difficulty = intent.getIntExtra(MainActivity.DIFF_KEY, 0);
+
+        switch (difficulty) {
+            case 0:
+                numChoices = 2;
+                break;
+            case 1:
+                numChoices = 4;
+                break;
+            case 2:
+                numChoices = 6;
+                break;
+        }
+
+        fileNameList = new ArrayList<String>();
+        quizWordList = new ArrayList<String>();
+        choiceWords = new ArrayList<String>();
+
+        random = new Random();
+        handler = new Handler();
+
+        getImageFileName();
+    }
+
+    private void getImageFileName() {
+        
     }
 
     @Override
